65de6ab24f40f64a1e50f474b29014318d66a9b3
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-10 18:33:10 -0400

    Update main.py

--- PythonBasics/BuildEmployeeProfileGenerator/main.py
+++ PythonBasics/BuildEmployeeProfileGenerator/main.py
@@ -1,4 +1,8 @@
 
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
+address += ', Apartment 4B'
+employee_age = 28
 employee_info = full_name + ' is ' + str(employee_age) + ' years old'
 print(employee_info)
 experience_years = 5
