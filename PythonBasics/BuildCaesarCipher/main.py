e4cd1deb2a2181c883d0b6e4a8ba22ed5f413000
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-15 20:59:59 -0400

    Update main.py

--- PythonBasics/BuildCaesarCipher/main.py
+++ PythonBasics/BuildCaesarCipher/main.py
@@ -1,1 +1,20 @@
 
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+
+encrypted_text = 'Pbhentr vf sbhaq va hayvxryl cynprf.'
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
