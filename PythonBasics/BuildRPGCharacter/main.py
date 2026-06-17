4f5f51d02f271c4abd80028d551f10aaef895dd7
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-17 14:27:27 -0400

    Update main.py

--- PythonBasics/BuildRPGCharacter/main.py
+++ PythonBasics/BuildRPGCharacter/main.py
@@ -1,6 +1,13 @@
 
 
-
+ elif len(name) > 10:
+        return 'The character name is too long'
+    elif ' ' in name:
+        return 'The character name should not contain spaces'
+    elif not all(isinstance(stat, int) for stat in (strength, intelligence, charisma)):
+        return 'All stats should be integers'
+    elif strength < 1 or intelligence < 1 or charisma < 1:
+        return 'All stats should be no less than 1'
 elif strength > 4 or intelligence > 4 or charisma > 4:
         return 'All stats should be no more than 4'
     elif strength + intelligence + charisma != 7:
