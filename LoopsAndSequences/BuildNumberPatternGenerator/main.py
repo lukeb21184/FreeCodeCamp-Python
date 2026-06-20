a141080c4d41c8f2395f895b99e698b7c8f2a440
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-20 15:43:51 -0400

    Update main.py

--- LoopsAndSequences/BuildNumberPatternGenerator/main.py
+++ LoopsAndSequences/BuildNumberPatternGenerator/main.py
@@ -1,9 +1,12 @@
-     if not isinstance(n, int):
+def number_pattern(n):
+    if not isinstance(n, int):
         return "Argument must be an integer value."
-     if n < 1:
-            return "Argument must be an integer greater than 0."
+    
+    if n < 1:
+        return "Argument must be an integer greater than 0."
     
     result = ""
+    
     for i in range(1, n + 1):
         result += str(i)
         
