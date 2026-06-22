9547a6c39418d4acb12948418cdf9824c82594c5
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-22 17:54:51 -0400

    Add patient records to main.py

--- DictionariesAndSets/BuildMedicalDataValidator/main.py
+++ DictionariesAndSets/BuildMedicalDataValidator/main.py
@@ -1,4 +1,30 @@
 
+   {
+        'patient_id': 'p1002',
+        'age': 47,
+        'gender': 'male',
+        'diagnosis': 'Type 2 Diabetes',
+        'medications': ['Metformin', 'Insulin'],
+        'last_visit_id': 'v2302',
+    },
+    {
+        'patient_id': 'P1003',
+        'age': 29,
+        'gender': 'female',
+        'diagnosis': 'Asthma',
+        'medications': ['Albuterol'],
+        'last_visit_id': 'v2303',
+    },
+    {
+        'patient_id': 'p1004',
+        'age': 56,
+        'gender': 'Male',
+        'diagnosis': 'Chronic Back Pain',
+        'medications': ['Ibuprofen', 'Physical Therapy'],
+        'last_visit_id': 'V2304',
+    }
+]
+
 def find_invalid_records(
     patient_id, age, gender, diagnosis, medications, last_visit_id
 ):
