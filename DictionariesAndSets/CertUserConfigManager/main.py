e8945274f06bcbf18ac48e2ba7595cca00289ac0
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-24 13:19:01 -0400

    Update main.py

--- DictionariesAndSets/CertUserConfigManager/main.py
+++ DictionariesAndSets/CertUserConfigManager/main.py
@@ -1,1 +1,7 @@
+def add_setting(settings, setting):
+    key, value = setting
+    key = key.lower()
+    value = value.lower()
 
+    if key in settings:
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
