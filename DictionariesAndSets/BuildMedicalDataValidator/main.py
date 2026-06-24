a1b9672c5b1a65e53117e886a63749f241cbfcba
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-23 23:45:14 -0400

    Implement validation for medical record format
    Add validation checks for medical record dictionaries.

--- DictionariesAndSets/BuildMedicalDataValidator/main.py
+++ DictionariesAndSets/BuildMedicalDataValidator/main.py
@@ -1,1 +1,28 @@
 
+  for index, dictionary in enumerate(data):
+        if not isinstance(dictionary, dict):
+            print(f'Invalid format: expected a dictionary at position {index}.')
+            is_invalid = True
+            continue
+
+        if set(dictionary.keys()) != key_set:
+            print(
+                f'Invalid format: {dictionary} at position {index} has missing and/or invalid keys.'
+            )
+            is_invalid = True
+            continue
+
+        invalid_records = find_invalid_records(**dictionary)
+        
+
+        for key in invalid_records:
+            print(
+            f"Unexpected format '{key}: {dictionary[key]}' at position {index}.")
+            is_invalid = True
+
+    if is_invalid:
+        return False
+    print('Valid format.')
+    return True
+
+validate(medical_records)
