2eb11e533b3a971a8d165ab7528b4c68ccb26733
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-06-24 13:20:01 -0400

    Update main.py

--- DictionariesAndSets/CertUserConfigManager/main.py
+++ DictionariesAndSets/CertUserConfigManager/main.py
@@ -14,3 +14,9 @@ def update_setting(settings, setting):
     key, value = setting
     key = key.lower()
     value = value.lower()
+    
+    if key in settings:
+        settings[key] = value
+        return f"Setting '{key}' updated to '{value}' successfully!"
+
+    return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
