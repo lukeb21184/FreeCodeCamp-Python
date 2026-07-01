8c8a4225e41b63f5e4b33190bd58e74d9d4c046b
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-07-01 17:44:56 -0400

    Add constructor to MusicalInstrument class

--- ClassesAndObjects/MusicalInstrumentInventory/main.py
+++ ClassesAndObjects/MusicalInstrumentInventory/main.py
@@ -1,4 +1,8 @@
-  def play(self):
+def __init__(self, name, instrument_type):
+        self.name = name
+        self.instrument_type = instrument_type
+
+def play(self):
         print(f"The {self.name} is fun to play!")
 
 def get_fact(self):
