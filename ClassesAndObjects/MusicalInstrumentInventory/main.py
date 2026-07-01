766c7dac868a5fca8b8c04639ac7b65fa25db2b0
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-07-01 17:44:33 -0400

    Add play method to MusicalInstrument class

--- ClassesAndObjects/MusicalInstrumentInventory/main.py
+++ ClassesAndObjects/MusicalInstrumentInventory/main.py
@@ -1,4 +1,7 @@
-   def get_fact(self):
+  def play(self):
+        print(f"The {self.name} is fun to play!")
+
+def get_fact(self):
         return f"The {self.name} is part of the {self.instrument_type} family of instruments."
 
 instrument_1 = MusicalInstrument("Oboe", "woodwind")
