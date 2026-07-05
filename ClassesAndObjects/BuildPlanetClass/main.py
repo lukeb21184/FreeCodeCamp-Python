cacc52664f28f53d69fdf526e2f145169aede3b8
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-07-05 15:30:18 -0400

    Instantiate Planet objects for Earth, Mars, and Jupiter

--- ClassesAndObjects/BuildPlanetClass/main.py
+++ ClassesAndObjects/BuildPlanetClass/main.py
@@ -1,4 +1,8 @@
 
+planet_1 = Planet("Earth", "Terrestrial", "Sun")
+planet_2 = Planet("Mars", "Terrestrial", "Sun")
+planet_3 = Planet("Jupiter", "Gas Giant", "Sun")
+
 print(planet_1)
 print(planet_2)
 print(planet_3)
