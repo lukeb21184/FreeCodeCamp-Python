2cb86679d9143cadb5ecfa6110c219be94f89fa9
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-07-07 13:07:39 -0400

    Add method to list emails in inbox

--- ClassesAndObjects/BuildEmailSimulator/main.py
+++ ClassesAndObjects/BuildEmailSimulator/main.py
@@ -1,3 +1,11 @@
+    def list_emails(self):
+        if not self.emails:
+            print('Your inbox is empty.\n')
+            return
+        print('\nYour Emails:')
+        for i, email in enumerate(self.emails, start=1):
+            print(f'{i}. {email}')
+            
     def read_email(self, index):
         if not self.emails:
             print('Inbox is empty.\n')
