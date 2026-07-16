852bcff2f4213af5c4fac5d9c7986f2f0be4e56d
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-07-16 19:50:05 -0400

    Add constructor and mark_as_read method to email class

--- ClassesAndObjects/BuildEmailSimulator/main.py
+++ ClassesAndObjects/BuildEmailSimulator/main.py
@@ -1,4 +1,14 @@
-    def display_full_email(self):
+    def __init__(self, sender, receiver, subject, body):
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.body = body
+        self.read = False
+
+    def mark_as_read(self):
+        self.read = True
+
+def display_full_email(self):
         self.mark_as_read()
         print("\n--- Email ---")
         print(f"From: {self.sender.name}")
