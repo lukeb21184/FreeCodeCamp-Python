a385fb365df0792a6e5ac00842b82d6dfe5cff60
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-09-22 08:20:41 -0400

    Add delete_email method to manage inbox emails

--- ClassesAndObjects/BuildEmailSimulator/main.py
+++ ClassesAndObjects/BuildEmailSimulator/main.py
@@ -55,3 +55,14 @@ class Inbox:
             print('Invalid email number.\n')
             return
         self.emails[actual_index].display_full_email()
+
+    def delete_email(self, index):
+        if not self.emails:
+            print('Inbox is empty.\n')
+            return
+        actual_index = index - 1
+        if actual_index < 0 or actual_index >= len(self.emails):
+            print('Invalid email number.\n')
+            return
+        del self.emails[actual_index]
+        print('Email deleted.\n')
