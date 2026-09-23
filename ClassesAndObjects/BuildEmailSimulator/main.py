d7ce5e691af5d46529820dc4ce4a81837b709c60
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-09-23 09:25:11 -0400

    Implement inbox check, read, and delete for Ramy
    Added email inbox functionality for user Ramy.

--- ClassesAndObjects/BuildEmailSimulator/main.py
+++ ClassesAndObjects/BuildEmailSimulator/main.py
@@ -88,6 +88,9 @@ def main():
     
     tory.send_email(ramy, 'Hello', 'Hi Ramy, just saying hello!')
     ramy.send_email(tory, 'Re: Hello', 'Hi Tory, hope you are fine.')
-    
+    ramy.check_inbox()
+    ramy.read_email(1)
+    ramy.delete_email(1)
+    ramy.check_inbox()
 if __name__ == '__main__':
     main()
