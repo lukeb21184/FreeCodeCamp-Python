0095f28c6141dd330fda7272414410d64e03235d
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2026-09-24 07:38:42 -0400

    Implement deposit and withdraw methods
    Added deposit and withdraw methods to Category class.

--- Build-Budget-App-Cert/main.py
+++ Build-Budget-App-Cert/main.py
@@ -2,3 +2,11 @@ class Category:
     def __init__(self, name):
         self.name = name
         self.ledger = []
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        return False
